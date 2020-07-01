25e00b217fe7fe611abb2663d55c73e78c5ec2f8
Erik De Luna <codingerik1.gmail.com>
2020-07-01 14:33:20 -0500

    fixed margin in cards

--- Develop/chalk/colors.js
+++ Develop/chalk/colors.js
@@ -1,6 +1,6 @@
 const chalkPipe = require('chalk-pipe');
 
-let blue = chalkPipe('blue.bold.'); 
+let blue = chalkPipe('blue.bold'); 
 let red = chalkPipe('red.bold'); 
 let purple = chalkPipe('purple.bold');
 let green = chalkPipe('green.bold');
